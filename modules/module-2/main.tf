b3492ccd14d6bb755a7b6a9ed255d177a50dc1f6
l4wsingh <37662195+l4wsingh@users.noreply.github.com>
2024-12-10 13:35:04 +0530

    Update main.tf

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -1,3 +1,5 @@
+##Original code
+
 terraform {
   required_providers {
     aws = {
@@ -348,17 +350,37 @@ data "aws_ami" "ecs_optimized_ami" {
 
 
 
-resource "aws_launch_configuration" "ecs_launch_config" {
+resource "aws_launch_template" "ecs_launch_template" {
+  name_prefix          = "ecs-launch-template-"
   image_id             = data.aws_ami.ecs_optimized_ami.id
   iam_instance_profile = aws_iam_instance_profile.ecs-instance-profile.name
   security_groups      = [aws_security_group.ecs_sg.id]
   user_data            = data.template_file.user_data.rendered
   instance_type        = "t2.micro"
+
+  iam_instance_profile {
+  name = aws_iam_instance_profile.ecs-instance-profile.name
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
 resource "aws_autoscaling_group" "ecs_asg" {
   name                 = "ECS-lab-asg"
   vpc_zone_identifier  = [aws_subnet.lab-subnet-public-1.id]
-  launch_configuration = aws_launch_configuration.ecs_launch_config.name
+  launch_template {
+    id      = aws_launch_template.ecs_launch_template.id
+    version = "$Latest"
+  }
   desired_capacity     = 1
   min_size             = 0
   max_size             = 1
