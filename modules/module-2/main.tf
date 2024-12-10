847324b99ae1c9e08ef38d582ad170d7bf17860c
l4wsingh <37662195+l4wsingh@users.noreply.github.com>
2024-12-10 13:18:35 +0530

    Update main.tf

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -26,29 +26,19 @@ resource "aws_vpc" "lab-vpc" {
     Name = "AWS_GOAT_VPC"
   }
 }
-
 resource "aws_subnet" "lab-subnet-public-1" {
   vpc_id                  = aws_vpc.lab-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[0]
 }
-
-resource "aws_subnet" "lab-subnet-public-1b" {
-  vpc_id                  = aws_vpc.lab-vpc.id
-  cidr_block              = "10.0.128.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = true
-}
-
 resource "aws_internet_gateway" "my_vpc_igw" {
   vpc_id = aws_vpc.lab-vpc.id
   tags = {
     Name = "My VPC - Internet Gateway"
   }
 }
-
-resource "aws_route_table" "my_vpc_public_rt" {
+resource "aws_route_table" "my_vpc_us_east_1_public_rt" {
   vpc_id = aws_vpc.lab-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
@@ -56,19 +46,104 @@ resource "aws_route_table" "my_vpc_public_rt" {
   }
 
   tags = {
-    Name = "Public Subnet Route Table"
+    Name = "Public Subnet Route Table."
   }
 }
 
-resource "aws_route_table_association" "my_vpc_subnet_1a_public" {
+resource "aws_route_table_association" "my_vpc_us_east_1a_public" {
   subnet_id      = aws_subnet.lab-subnet-public-1.id
-  route_table_id = aws_route_table.my_vpc_public_rt.id
+  route_table_id = aws_route_table.my_vpc_us_east_1_public_rt.id
+}
+resource "aws_subnet" "lab-subnet-public-1b" {
+  vpc_id                  = aws_vpc.lab-vpc.id
+  cidr_block              = "10.0.128.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+}
+resource "aws_route_table_association" "my_vpc_us_east_1b_public" {
+  subnet_id      = aws_subnet.lab-subnet-public-1b.id
+  route_table_id = aws_route_table.my_vpc_us_east_1_public_rt.id
 }
 
-resource "aws_route_table_association" "my_vpc_subnet_1b_public" {
-  subnet_id      = aws_subnet.lab-subnet-public-1b.id
-  route_table_id = aws_route_table.my_vpc_public_rt.id
+resource "aws_security_group" "ecs_sg" {
+  name        = "ECS-SG"
+  description = "SG for cluster created from terraform"
+  vpc_id      = aws_vpc.lab-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+# Create Database Subnet Group
+# terraform aws db subnet group
+resource "aws_db_subnet_group" "database-subnet-group" {
+  name        = "database subnets"
+  subnet_ids  = [aws_subnet.lab-subnet-public-1.id, aws_subnet.lab-subnet-public-1b.id]
+  description = "Subnets for Database Instance"
+
+  tags = {
+    Name = "Database Subnets"
+  }
+}
+
+# Create Security Group for the Database
+# terraform aws create security group
+
+resource "aws_security_group" "database-security-group" {
+  name        = "Database Security Group"
+  description = "Enable MYSQL Aurora access on Port 3306"
+  vpc_id      = aws_vpc.lab-vpc.id
+
+  ingress {
+    description     = "MYSQL/Aurora Access"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.ecs_sg.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-db-sg"
+  }
+
+}
+
+# Create Database Instance Restored from DB Snapshots
+# terraform aws db instance
+resource "aws_db_instance" "database-instance" {
+  identifier             = "aws-goat-db"
+  allocated_storage      = 10
+  instance_class         = "db.t3.micro"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  username               = "root"
+  password               = "T2kVB3zgeN3YbrKS"
+  parameter_group_name   = "default.mysql5.7"
+  skip_final_snapshot    = true
+  availability_zone      = "us-east-1a"
+  db_subnet_group_name   = aws_db_subnet_group.database-subnet-group.name
+  vpc_security_group_ids = [aws_security_group.database-security-group.id]
+}
+
+
 
 resource "aws_security_group" "load_balancer_security_group" {
   name        = "Load-Balancer-SG"
@@ -88,82 +163,37 @@ resource "aws_security_group" "load_balancer_security_group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "aws-goat-m2-sg"
   }
 }
 
-# Create DB Subnet Group
-resource "aws_db_subnet_group" "database-subnet-group" {
-  name        = "database subnets"
-  subnet_ids  = [aws_subnet.lab-subnet-public-1.id, aws_subnet.lab-subnet-public-1b.id]
-  description = "Subnets for Database Instance"
 
-  tags = {
-    Name = "Database Subnets"
-  }
-}
 
-resource "aws_security_group" "database-security-group" {
-  name        = "Database Security Group"
-  description = "Enable MYSQL Aurora access on Port 3306"
-  vpc_id      = aws_vpc.lab-vpc.id
-
-  ingress {
-    description     = "MYSQL/Aurora Access"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "rds-db-sg"
-  }
-}
-
-# RDS Instance
-resource "aws_db_instance" "database-instance" {
-  identifier             = "aws-goat-db"
-  allocated_storage      = 10
-  instance_class         = "db.t3.micro"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  username               = "root"
-  password               = "T2kVB3zgeN3YbrKS"
-  parameter_group_name   = "default.mysql5.7"
-  skip_final_snapshot    = true
-  availability_zone      = "us-east-1a"
-  db_subnet_group_name   = aws_db_subnet_group.database-subnet-group.name
-  vpc_security_group_ids = [aws_security_group.database-security-group.id]
-}
-
-# IAM roles and policies
 resource "aws_iam_role" "ecs-instance-role" {
   name                 = "ecs-instance-role"
-  assume_role_policy   = jsonencode({
-    Version : "2012-10-17",
-    Statement : [{
-      Effect   = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" },
-      Action   = "sts:AssumeRole"
-    }]
+  path                 = "/"
+  permissions_boundary = aws_iam_policy.instance_boundary_policy.arn
+  assume_role_policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
+
+resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment-1" {
   role       = aws_iam_role.ecs-instance-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
-
 resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment-2" {
   role       = aws_iam_role.ecs-instance-role.name
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
@@ -174,7 +204,178 @@ resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment-3" {
   policy_arn = aws_iam_policy.ecs_instance_policy.arn
 }
 
-# ECS Task Definition
+resource "aws_iam_policy" "ecs_instance_policy" {
+  name = "aws-goat-instance-policy"
+  policy = jsonencode({
+    "Statement" : [
+      {
+        "Action" : [
+          "ssm:*",
+          "ssmmessages:*",
+          "ec2:RunInstances",
+          "ec2:Describe*"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Sid" : "Pol1"
+      }
+    ],
+    "Version" : "2012-10-17"
+  })
+}
+
+resource "aws_iam_policy" "instance_boundary_policy" {
+  name = "aws-goat-instance-boundary-policy"
+  policy = jsonencode({
+    "Statement" : [
+      {
+        "Action" : [
+          "iam:List*",
+          "iam:Get*",
+          "iam:PassRole",
+          "iam:PutRole*",
+          "ssm:*",
+          "ssmmessages:*",
+          "ec2:RunInstances",
+          "ec2:Describe*",
+          "ecs:*",
+          "ecr:*",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Sid" : "Pol1"
+      }
+    ],
+    "Version" : "2012-10-17"
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2-deployer-profile" {
+  name = "ec2Deployer"
+  path = "/"
+  role = aws_iam_role.ec2-deployer-role.id
+}
+resource "aws_iam_role" "ec2-deployer-role" {
+  name = "ec2Deployer-role"
+  path = "/"
+  assume_role_policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ec2_deployer_admin_policy" {
+  name = "ec2DeployerAdmin-policy"
+  policy = jsonencode({
+    "Statement" : [
+      {
+        "Action" : [
+          "*"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Sid" : "Policy1"
+      }
+    ],
+    "Version" : "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-deployer-role-attachment" {
+  role       = aws_iam_role.ec2-deployer-role.name
+  policy_arn = aws_iam_policy.ec2_deployer_admin_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ecs-instance-profile" {
+  name = "ecs-instance-profile"
+  path = "/"
+  role = aws_iam_role.ecs-instance-role.id
+}
+resource "aws_iam_role" "ecs-task-role" {
+  name = "ecs-task-role"
+  path = "/"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ecs-tasks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-role-attachment" {
+  role       = aws_iam_role.ecs-task-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+resource "aws_iam_role_policy_attachment" "ecs-task-role-attachment-2" {
+  role       = aws_iam_role.ecs-task-role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment-ssm" {
+  role       = aws_iam_role.ecs-instance-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
+data "aws_ami" "ecs_optimized_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-2.0.202*-x86_64-ebs"]
+  }
+}
+
+
+
+resource "aws_launch_configuration" "ecs_launch_config" {
+  image_id             = data.aws_ami.ecs_optimized_ami.id
+  iam_instance_profile = aws_iam_instance_profile.ecs-instance-profile.name
+  security_groups      = [aws_security_group.ecs_sg.id]
+  user_data            = data.template_file.user_data.rendered
+  instance_type        = "t2.micro"
+}
+resource "aws_autoscaling_group" "ecs_asg" {
+  name                 = "ECS-lab-asg"
+  vpc_zone_identifier  = [aws_subnet.lab-subnet-public-1.id]
+  launch_configuration = aws_launch_configuration.ecs_launch_config.name
+  desired_capacity     = 1
+  min_size             = 0
+  max_size             = 1
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "ecs-lab-cluster"
+
+  tags = {
+    name = "ecs-cluster-name"
+  }
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/resources/ecs/user_data.tpl")
+}
+
 resource "aws_ecs_task_definition" "task_definition" {
   container_definitions    = data.template_file.task_definition_json.rendered
   family                   = "ECS-Lab-Task-definition"
@@ -185,17 +386,24 @@ resource "aws_ecs_task_definition" "task_definition" {
   task_role_arn            = aws_iam_role.ecs-task-role.arn
 
   pid_mode = "host"
+  volume {
+    name      = "modules"
+    host_path = "/lib/modules"
+  }
+  volume {
+    name      = "kernels"
+    host_path = "/usr/src/kernels"
+  }
 }
 
 data "template_file" "task_definition_json" {
   template = file("${path.module}/resources/ecs/task_definition.json")
+  depends_on = [
+    null_resource.rds_endpoint
+  ]
 }
 
-# ECS Cluster and Service
-resource "aws_ecs_cluster" "cluster" {
-  name = "ecs-lab-cluster"
-  tags = { name = "ecs-cluster-name" }
-}
+
 
 resource "aws_ecs_service" "worker" {
   name                              = "ecs_service_worker"
@@ -209,6 +417,7 @@ resource "aws_ecs_service" "worker" {
     container_name   = "aws-goat-m2"
     container_port   = 80
   }
+  depends_on = [aws_lb_listener.listener]
 }
 
 resource "aws_alb" "application_load_balancer" {
@@ -218,7 +427,9 @@ resource "aws_alb" "application_load_balancer" {
   subnets            = [aws_subnet.lab-subnet-public-1.id, aws_subnet.lab-subnet-public-1b.id]
   security_groups    = [aws_security_group.load_balancer_security_group.id]
 
-  tags = { Name = "aws-goat-m2-alb" }
+  tags = {
+    Name = "aws-goat-m2-alb"
+  }
 }
 
 resource "aws_lb_target_group" "target_group" {
@@ -227,6 +438,10 @@ resource "aws_lb_target_group" "target_group" {
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = aws_vpc.lab-vpc.id
+
+  tags = {
+    Name = "aws-goat-m2-tg"
+  }
 }
 
 resource "aws_lb_listener" "listener" {
@@ -240,7 +455,7 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
-# Secrets Manager
+
 resource "aws_secretsmanager_secret" "rds_creds" {
   name                    = "RDS_CREDS"
   recovery_window_in_days = 0
@@ -248,7 +463,53 @@ resource "aws_secretsmanager_secret" "rds_creds" {
 
 resource "aws_secretsmanager_secret_version" "secret_version" {
   secret_id     = aws_secretsmanager_secret.rds_creds.id
-  secret_string = jsonencode({ "username" = "root", "password" = "T2kVB3zgeN3YbrKS" })
+  secret_string = <<EOF
+   {
+    "username": "root",
+    "password": "T2kVB3zgeN3YbrKS"
+   }
+EOF
+}
+
+resource "null_resource" "rds_endpoint" {
+  provisioner "local-exec" {
+    command     = <<EOF
+RDS_URL="${aws_db_instance.database-instance.endpoint}"
+RDS_URL=$${RDS_URL::-5}
+sed -i "s,RDS_ENDPOINT_VALUE,$RDS_URL,g" ${path.module}/resources/ecs/task_definition.json
+EOF
+    interpreter = ["/bin/bash", "-c"]
+  }
+
+  depends_on = [
+    aws_db_instance.database-instance
+  ]
+}
+
+resource "null_resource" "cleanup" {
+  provisioner "local-exec" {
+    command     = <<EOF
+RDS_URL="${aws_db_instance.database-instance.endpoint}"
+RDS_URL=$${RDS_URL::-5}
+sed -i "s,$RDS_URL,RDS_ENDPOINT_VALUE,g" ${path.module}/resources/ecs/task_definition.json
+EOF
+    interpreter = ["/bin/bash", "-c"]
+  }
+
+  depends_on = [
+    null_resource.rds_endpoint, aws_ecs_task_definition.task_definition
+  ]
+}
+
+
+/* Creating a S3 Bucket for Terraform state file upload. */
+resource "aws_s3_bucket" "bucket_tf_files" {
+  bucket        = "do-not-delete-awsgoat-state-files-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
+  tags = {
+    Name        = "Do not delete Bucket"
+    Environment = "Dev"
+  }
 }
 
 output "ad_Target_URL" {
